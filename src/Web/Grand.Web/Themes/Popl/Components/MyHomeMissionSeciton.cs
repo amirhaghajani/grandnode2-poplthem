7433d08d3569a2cd6362b35e6f235007eb38d912
amirhaghajani <amirhaghajani@gmail.com>
2022-06-09 05:13:44 +0430

    FAQ seciton

---
+++ src/Web/Grand.Web/Themes/Popl/Components/MyHomeMissionSeciton.cs
@@ -0,0 +1,19 @@
+﻿using Grand.Business.Common.Interfaces.Security;
+using Grand.Business.Common.Services.Security;
+using Grand.Infrastructure;
+using Grand.Web.Common.Components;
+using Grand.Web.Features.Models.Catalog;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grand.Web.Themes.Popl.Components
+{
+    public class MyHomeMissionSeciton : BaseViewComponent
+    {
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            IViewComponentResult answer = View();
+            return Task.FromResult(answer);
+        }
+    }
+}
