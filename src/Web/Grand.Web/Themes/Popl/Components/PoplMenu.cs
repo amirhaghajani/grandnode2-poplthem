6c55d30bcc07259ae49755e7f5c9dbd4c3d3a1c4
amirhaghajani <amirhaghajani@gmail.com>
2022-06-12 03:10:45 +0430

    separate popl layout from other section

---
+++ src/Web/Grand.Web/Themes/Popl/Components/PoplMenu.cs
@@ -0,0 +1,42 @@
+﻿using Grand.Business.Common.Interfaces.Security;
+using Grand.Business.Common.Services.Security;
+using Grand.Infrastructure;
+using Grand.Web.Common.Components;
+using Grand.Web.Features.Models.Catalog;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grand.Web.ViewComponents
+{
+    public class PoplMenuViewComponent : BaseViewComponent
+    {
+        private readonly IMediator _mediator;
+        private readonly IWorkContext _workContext;
+        private readonly IPermissionService _permissionService;
+
+        public PoplMenuViewComponent(
+            IMediator mediator,
+            IWorkContext workContext,
+            IPermissionService permissionService)
+        {
+            _mediator = mediator;
+            _workContext = workContext;
+            _permissionService = permissionService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (!await _permissionService.Authorize(StandardPermission.PublicStoreAllowNavigation, _workContext.CurrentCustomer))
+                return Content("");
+
+            var model = await _mediator.Send(new GetMenu()
+            {
+                Customer = _workContext.CurrentCustomer,
+                Language = _workContext.WorkingLanguage,
+                Store = _workContext.CurrentStore
+            });
+
+            return View(model);
+        }
+    }
+}
