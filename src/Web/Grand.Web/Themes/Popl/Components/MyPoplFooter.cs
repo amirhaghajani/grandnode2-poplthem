50e535e56b095a2db8ccb6cae11838bd8767b0d4
amirhaghajani <amirhaghajani@gmail.com>
2022-07-12 03:34:20 +0430

    فارسی سازی شروع شد

---
+++ src/Web/Grand.Web/Themes/Popl/Components/MyPoplFooter.cs
@@ -0,0 +1,16 @@
+﻿using Grand.Web.Common.Components;
+using Grand.Web.Features.Models.Catalog;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grand.Web.Themes.Popl.Components
+{
+    public class MyPoplFooter : BaseViewComponent
+    {
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            IViewComponentResult answer = View();
+            return Task.FromResult(answer);
+        }
+    }
+}
