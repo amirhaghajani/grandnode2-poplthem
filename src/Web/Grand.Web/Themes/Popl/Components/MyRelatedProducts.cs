6d7ab53d84ce57834856f0dec667847279eddff2
Amir h Aghajani <amirhaghajani@gmail.com>
2022-10-09 02:56:34 +0330

    working on cart page

---
+++ src/Web/Grand.Web/Themes/Popl/Components/MyRelatedProducts.cs
@@ -0,0 +1,56 @@
+﻿using Grand.Business.Catalog.Interfaces.Products;
+using Grand.Domain.Catalog;
+using Grand.Web.Common.Components;
+using Grand.Web.Features.Models.Products;
+using Grand.Web.Themes.Popl.Features.Models.Products;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grand.Web.Components
+{
+    public class MyRelatedProductsViewComponent : BaseViewComponent
+    {
+        #region Fields
+        private readonly IProductService _productService;
+        private readonly IMediator _mediator;
+
+        private readonly CatalogSettings _catalogSettings;
+        #endregion
+
+        #region Constructors
+
+        public MyRelatedProductsViewComponent(
+            IProductService productService,
+            IMediator mediator,
+            CatalogSettings catalogSettings)
+        {
+            _productService = productService;
+            _mediator = mediator;
+            _catalogSettings = catalogSettings;
+        }
+
+        #endregion
+
+        #region Invoker
+
+        public async Task<IViewComponentResult> InvokeAsync(string productId, int? productThumbPictureSize)
+        {
+            var productIds = (await _productService.GetProductById(productId)).RelatedProducts.OrderBy(x => x.DisplayOrder).Select(x => x.ProductId2).ToArray();
+
+            //load products
+            var products = await _productService.GetProductsByIds(productIds);
+
+            var model = await _mediator.Send(new MyGetProductOverview() {
+                PreparePictureModel = true,
+                PreparePriceModel = true,
+                PrepareSpecificationAttributes = _catalogSettings.ShowSpecAttributeOnCatalogPages,
+                ProductThumbPictureSize = productThumbPictureSize,
+                Products = products
+            });
+
+            return View(model);
+        }
+
+        #endregion
+    }
+}
